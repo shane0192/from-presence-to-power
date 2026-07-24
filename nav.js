787afb7b482d9e782fb4cbba6d70efd9e39ffdd2
Shane <shane@weareleanmean.com>
2026-07-24 08:27:42 -0700

    Nav: rename Book Extras to Find Your Power, add About Rashad dropdown with rashadrobinson.com

--- nav.js
+++ nav.js
@@ -12,8 +12,14 @@
       '<li><a href="index.html#book"' + active('index.html') + '>About the Book</a></li>' +
       '<li><a href="events.html"' + active('events.html') + '>Upcoming Events</a></li>' +
       '<li><a href="https://www.rashadrobinson.com/press" target="_blank" rel="noopener">Press</a></li>' +
-      '<li><a href="index.html#rashad">About Rashad</a></li>' +
-      '<li><a href="fyp.html"' + active('fyp.html') + '>Book Extras</a></li>' +
+      '<li class="nav-dropdown">' +
+        '<a href="index.html#rashad">About Rashad</a>' +
+        '<ul class="nav-dropdown-menu">' +
+          '<li><a href="index.html#rashad">Bio</a></li>' +
+          '<li><a href="https://www.rashadrobinson.com" target="_blank" rel="noopener">rashadrobinson.com</a></li>' +
+        '</ul>' +
+      '</li>' +
+      '<li><a href="fyp.html"' + active('fyp.html') + '>Find Your Power</a></li>' +
       '<li><a href="https://www.penguinrandomhouse.com/books/676299/from-presence-to-power-by-rashad-robinson/" target="_blank" rel="noopener" class="nav-cta">Order Book</a></li>' +
     '</ul>' +
     '<button class="nav-toggle" id="navToggle" aria-label="Open menu" aria-expanded="false">' +
@@ -25,7 +31,8 @@
     '<a href="events.html">Upcoming Events</a>' +
     '<a href="https://www.rashadrobinson.com/press" target="_blank" rel="noopener">Press</a>' +
     '<a href="index.html#rashad">About Rashad</a>' +
-    '<a href="fyp.html">Book Extras</a>' +
+    '<a href="https://www.rashadrobinson.com" target="_blank" rel="noopener">rashadrobinson.com</a>' +
+    '<a href="fyp.html">Find Your Power</a>' +
     '<a href="https://www.penguinrandomhouse.com/books/676299/from-presence-to-power-by-rashad-robinson/" target="_blank" rel="noopener" class="mobile-menu-cta">Order the Book</a>';
 
   // Scroll: add .scrolled class
