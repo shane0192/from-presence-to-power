dc954508880c792cab1fae2d3a954003984f6355
Shane <shane@weareleanmean.com>
2026-08-16 12:02:29 -0700

    nav: replace Book Extras dropdown with single Appendix link

--- nav.js
+++ nav.js
@@ -22,7 +22,6 @@
           '<li><a href="https://www.rashadrobinson.com" target="_blank" rel="noopener">rashadrobinson.com</a></li>' +
         '</ul>' +
       '</li>' +
-      '<li><a href="/fyp"' + active('/fyp') + '>Find Your Power</a></li>' +
       '<li><a href="/appendix"' + active('/appendix') + '>Appendix</a></li>' +
       '<li><a href="https://www.penguinrandomhouse.com/books/676299/from-presence-to-power-by-rashad-robinson/" target="_blank" rel="noopener" class="nav-cta">Order Book</a></li>' +
     '</ul>' +
